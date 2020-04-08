c103d5913f1028a5de6ad5b004301c21ac90d101
Adam Beal <adamwbeal@gmail.com>
2020-04-08 18:50:50 -0400

    retry deploy

--- python/get_inspections.py
+++ python/get_inspections.py
@@ -3,11 +3,10 @@ import requests
 import json
 import datetime
 
-print('Starting at {}'.format(datetime.datetime.now()))
-r = requests.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=300000')
-r.status_code
+print('Starting at:                 {}'.format(datetime.datetime.now()))
+r = requests.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=1')
 data = r.json()
-print('Finished retrieving json at {}'.format(datetime.datetime.now()))
+print('Finished retrieving json at: {}'.format(datetime.datetime.now()))
 
 s3 = boto3.resource('s3')
 
@@ -15,4 +14,4 @@ filename = str(str(datetime.datetime.now())+'.json')
 obj = s3.Object('doh-inspection-storage',filename)
 obj.put(Body=json.dumps(data))
 
-print('Upload complete at {}'.format(datetime.datetime.now()))
+print('Upload complete at:          {}'.format(datetime.datetime.now()))
