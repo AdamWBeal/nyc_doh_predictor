2a8ba90227fc46b8e39c654a2b2878f12eed13ef
Adam Beal <adamwbeal@gmail.com>
2020-04-08 22:54:33 -0400

    up limit

--- clock.py
+++ clock.py
@@ -9,17 +9,19 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('Starting at:                 {}'.format(datetime.datetime.now()))
-    r = requests.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=400000')
-    data = r.json()
-    print('Finished retrieving json at: {}'.format(datetime.datetime.now()))
+    try:
+        print('Starting at:                 {}'.format(datetime.datetime.now()))
+        r = requests.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=400000')
+        data = r.json()
+        print('Finished retrieving json at: {}'.format(datetime.datetime.now()))
 
-    s3 = boto3.resource('s3')
+        s3 = boto3.resource('s3')
 
-    filename = str(str(datetime.datetime.now())+'.json')
-    obj = s3.Object('doh-inspection-storage',filename)
-    obj.put(Body=json.dumps(data))
+        filename = str(str(datetime.datetime.now())+'.json')
+        obj = s3.Object('doh-inspection-storage',filename)
+        obj.put(Body=json.dumps(data))
 
-    print('Upload complete at:          {}'.format(datetime.datetime.now()))
+        print('Upload complete at:          {}'.format(datetime.datetime.now()))
+    except Exception as e: print(e)
 
 sched.start()
