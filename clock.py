679f98eec3bdc8e76a10955c27a792c55a354bf7
Adam Beal <adamwbeal@gmail.com>
2020-04-09 09:12:07 -0400

    adjusted script timing interval

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=5)
 def timed_job():
     try:
         print('Starting at:                 {}'.format(datetime.datetime.now()))
