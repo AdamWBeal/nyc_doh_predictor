6c39401736077418a5b592a5725eefc0975726b4
Brenton Morris <brentonamorris@gmail.com>
2020-04-08 21:09:25 -0400

    Add clock.py

---
+++ clock.py
@@ -0,0 +1,25 @@
+import boto3
+import requests
+import json
+import datetime
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.get_inspections('interval', minutes=3)
+def get_inspections():
+    print('Starting at:                 {}'.format(datetime.datetime.now()))
+    r = requests.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=1')
+    data = r.json()
+    print('Finished retrieving json at: {}'.format(datetime.datetime.now()))
+
+    s3 = boto3.resource('s3')
+
+    filename = str(str(datetime.datetime.now())+'.json')
+    obj = s3.Object('doh-inspection-storage',filename)
+    obj.put(Body=json.dumps(data))
+
+    print('Upload complete at:          {}'.format(datetime.datetime.now()))
+
+sched.start()
